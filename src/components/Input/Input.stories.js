cad615d8cbf20277ac447ad3c2b73ffc3b984917
Joe Park-Kennaby <jparkkennaby@gmail.com>
2021-02-24 15:13:39 +0000

    bug fix - update incorrect input story export

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -10,7 +10,7 @@ export default Default;
 
 export const Small = () => <Input variant="small" placeholder="Small size"/>
 export const Medium = () => <Input variant="medium" placeholder="Medium Size"/>
-export const large = () => <Input variant="large" placeholder="large size"/>
+export const Large = () => <Input variant="large" placeholder="large size"/>
 
 // rename a Story
 // Small.storyName = 'Small Input';
