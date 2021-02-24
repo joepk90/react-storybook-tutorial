af0b0d9dd4d576e737a123d352b4eba79f3fbbbd
Joe Park-Kennaby <jparkkennaby@gmail.com>
2021-02-24 17:57:40 +0000

    11 - setup extra viewport options using the viewport addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,7 @@
 import {ThemeProvider, theme, CSSReset, Box} from '@chakra-ui/core'
 import { withConsole } from '@storybook/addon-console';
 import { addDecorator } from '@storybook/react';
+import {INITIAL_VIEWPORTS} from '@storybook/addon-viewport';
 
 // shows which component the console log was declared
 addDecorator((storyFn, context) => withConsole()(storyFn)(context));
@@ -11,6 +12,9 @@ export const parameters = {
     storySort: (a, b) =>
       a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
   },
+  viewport: {
+    viewports: INITIAL_VIEWPORTS
+  }
 }
 
 export const decorators = [
