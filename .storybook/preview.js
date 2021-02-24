a096fbfe248eb3ff0e7e942d868a9cae6d786b4a
Joe Park-Kennaby <jparkkennaby@gmail.com>
2021-02-24 16:39:03 +0000

    6.2 - convert glocal decorator theming logic to use stroybook 6 syntax

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,4 @@
-import { addDecorator } from '@storybook/react';
 import {ThemeProvider, theme, CSSReset, Box} from '@chakra-ui/core'
-
-addDecorator(story => (
-  <ThemeProvider theme={theme}>
-    <CSSReset />
-      <Box m="4">{story()}</Box>
-  </ThemeProvider>
-  )
-);
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -16,3 +7,14 @@ export const parameters = {
       a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
   },
 }
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+    <CSSReset />
+      <Box m="4">
+        <Story />
+      </Box>
+    </ThemeProvider>
+  )
+];
