da282e854f83f3f14d80c6b65b579574340fea74
Joe Park-Kennaby <jparkkennaby@gmail.com>
2021-02-24 15:19:09 +0000

    4.4 - setup base args functionality on Button Story

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -12,3 +12,10 @@ export const Primary = () => <Button variant="primary">Primary</Button>
 export const Secondary = () => <Button variant="secondary">Secondary</Button>
 export const Success = () => <Button variant="success">Success</Button>
 export const Danger = () => <Button variant="danger">Danger</Button>
+
+/**
+ * Args functionality
+ */
+const Template = args => <Button {...args} />
+
+export const PrimaryA = Template.bind({});
