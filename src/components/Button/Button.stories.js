11ec94e2e87de0d41ea632b17a00fa6b7de20a7a
Joe Park-Kennaby <jparkkennaby@gmail.com>
2021-02-24 15:28:56 +0000

    4.4 - setup example button stories using the args object. add documenting comment declaring args at the component level

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -3,7 +3,10 @@ import Button from './Button';
 
 const Default = {
     title: 'Form/Button',
-    component: Button
+    component: Button,
+    // args: {
+    //     children: 'Button' // optionally declare args at component level to apply to all Stories
+    // }
 }
 
 export default Default;
@@ -19,3 +22,19 @@ export const Danger = () => <Button variant="danger">Danger</Button>
 const Template = args => <Button {...args} />
 
 export const PrimaryA = Template.bind({});
+PrimaryA.args = {
+    variant: 'primary',
+    children: 'Primary Args'
+}
+
+export const SecondaryA = Template.bind({});
+SecondaryA.args = {
+    variant: 'secondary',
+    children: 'Secondary Args'
+}
+
+// example of reusing another Stories args
+// export const LongPrimaryA = Template.bind({})
+// LongPrimaryA.args = {
+//     ...PrimaryA.args
+// }
