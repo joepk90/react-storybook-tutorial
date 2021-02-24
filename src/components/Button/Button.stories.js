81685b76159eddd2863a85ab6a81ee734f8e3ccb
Joe Park-Kennaby <jparkkennaby@gmail.com>
2021-02-24 15:38:09 +0000

    4.5 - apply the Center Decorator to all the Button Stories

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from './Button';
+import Center from '../Center/Center';
 
 const Default = {
     title: 'Form/Button',
@@ -11,10 +12,10 @@ const Default = {
 
 export default Default;
 
-export const Primary = () => <Button variant="primary">Primary</Button>
-export const Secondary = () => <Button variant="secondary">Secondary</Button>
-export const Success = () => <Button variant="success">Success</Button>
-export const Danger = () => <Button variant="danger">Danger</Button>
+export const Primary = () => <Center><Button variant="primary">Primary</Button></Center>
+export const Secondary = () => <Center><Button variant="secondary">Secondary</Button></Center>
+export const Success = () => <Center><Button variant="success">Success</Button></Center>
+export const Danger = () => <Center><Button variant="danger">Danger</Button></Center>
 
 /**
  * Args functionality
